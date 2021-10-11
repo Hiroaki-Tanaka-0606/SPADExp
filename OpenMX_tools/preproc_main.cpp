2b8760a72d31971944e4bbc3a876c4ce55d382ed
Hiroaki Tanaka <hiroaki-tanaka@issp.u-tokyo.ac.jp>
2021-10-11 20:46:40 +0900

    remove comment in <MO.kpoint>

--- OpenMX_tools/preproc_main.cpp
+++ OpenMX_tools/preproc_main.cpp
@@ -258,8 +258,9 @@ int main(int argc, const char** argv){
 			p=xMin+dp*i;
 			// cout << p << " " << q << endl;
 			r_Kp=calcKp(p, q, r_X, r_Y, r_O, origin_frac, x_frac, y_frac, curved, kp);
-			sprintf(writeBuf, "%11.5f %11.5f %11.5f # r = %11.5f", kp[0], kp[1], kp[2], r_Kp);
-			output << writeBuf << endl;					
+			sprintf(writeBuf, "%11.5f %11.5f %11.5f", kp[0], kp[1], kp[2]);
+			output << writeBuf << endl;
+			printf("%s # r = %11.5f\n", writeBuf, r_Kp);
 		}
 	}
 	output << "MO.kpoint>" << endl;
