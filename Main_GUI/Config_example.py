ba92d685ebf92b52196fd3c46edb2ab93e6843ba
Hiroaki Tanaka <hiroaki-tanaka@issp.u-tokyo.ac.jp>
2021-11-21 16:48:46 +0900

    update Config_example

--- Main_GUI/Config_example.py
+++ Main_GUI/Config_example.py
@@ -12,6 +12,7 @@ tickLength=-30
 sigma_max=5
 
 Eh=27.2114 # (eV)
+au_ang=0.529177 # (Ang)
 
 # Pen for vLine and hLine
 pen1=(0, 255, 0)
@@ -19,7 +20,12 @@ pen2=(255, 0, 0)
 pen3=(255, 255, 0)
 gridAlpha=50 # 100 =max
 
-
-
 plot3D_minWidth=400
 plot3D_minHeight=400
+
+PAO_and_AO="/data/Ubuntu/calcPSF/GUI_tools/OpenMX_orbitals/PAO_and_AO_after_opt.hdf5"
+pen_AO=(0, 0, 255)
+pen_PAO=(0, 128, 255)
+pen_finalp1=(255, 128, 0)
+pen_finalm1=(128, 255, 0)
+
